39b73cb1ba53665a21063f325704d3d732f921d7
pradiknatama <pradiknatama@gmail.com>
2024-07-10 00:06:28 +0700

    add create and show all promo

--- controllers/cms/promoController.js
+++ controllers/cms/promoController.js
@@ -1,8 +1,24 @@
-const findAll = async (req, res, next) => {};
+const promoService = require('../../services/promoService')
+
+const findAll = async (req, res, next) => {
+    try {
+        const promo = await promoService.findAll(req.body);
+        res.status(201).json({message: "Promo", data: promo})
+    } catch (err) {
+        next(err)
+    }
+};
 
 const findOne = async (req, res, next) => {};
 
-const create = async (req, res, next) => {};
+const create = async (req, res, next) => {
+    try {
+        const promo = await promoService.create(req.body);
+        res.status(201).json({message: "Promo Created", data: promo})
+    } catch (err) {
+        next(err)
+    }
+};
 
 const update = async (req, res, next) => {};
 
