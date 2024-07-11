70bbf990993e00b3ea10f9dd6d152fdb948948a1
pradiknatama <pradiknatama@gmail.com>
2024-07-12 00:29:05 +0700

    cart findone and reset cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,36 @@
-const findOne = async (req, res, next) => {};
+const cartService = require('../services/cartService')
 
-const reset = async (req, res, next) => {};
+const findOne = async (req, res, next) => {
+    try {
+        const params = { user_id: req.loggedUser.id};
+        const cart = await cartService.findOne(params);
+        res.status(200).json({ message: "Cart : ", data: cart });
 
-const update = async (req, res, next) => {};
+    } catch (error) {
+        next (error);
+    }
+};
+
+const reset = async (req, res, next) => {
+    try {
+        const params = { user_id: req.loggedUser.id};
+        const reset_cart = await cartService.reset(params);
+        res.status(200).json({ message: reset_cart })
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const params = { user_id: req.loggedUser.id};
+        const cart = await cartService.update(params);
+        res.status(200).json({ message: "Cart Updated", data: cart });
+    } catch (error) {
+        next(error);
+    }
+};
 
 const deleteProduct = async (req, res, next) => {};
 
--- services/cartService.js
+++ services/cartService.js
@@ -1,9 +1,36 @@
-const findOne = async (params) => {};
+const prisma = require("../lib/prisma");
 
-const reset = async (params) => {};
+const findOne = async (params) => {
+    const user_id = params.user_id;
+    console.log(user_id);
+    const cart = await prisma.cart.findFirst({
+        where: {user_id:user_id},
+        include:{cart_details:true}
+    })
+    return cart;
+};
 
-const update = async (params) => {};
+const reset = async (params) => {
+    const user_id = params.user_id;
+    let message="";
+    const cart = await prisma.cart.findFirst({
+        where: {user_id:user_id},
+    });
+    const cart_detail = await prisma.cart_Detail.deleteMany({
+        where: {cart_id:cart.id},
+    });
+    if(cart_detail.count ==0){
+        message="All Shopping Cart Deleted";
+    }
+    return message;
+};
 
-const deleteProduct = async (params) => {};
+const update = async (params) => {
+    
+};
+
+const deleteProduct = async (params) => {
+
+};
 
 module.exports = { findOne, reset, update, deleteProduct };
