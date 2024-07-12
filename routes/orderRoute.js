e3ff7032fe597fd1d6b271341c26c5bba534c1b2
RR Sylva Prayuniza <sylvaroro@gmail.com>
2024-07-12 18:35:11 +0700

    fitur order

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
 
+router.post("/", orderController.create);
 router.get("/", orderController.findAll);
 router.get("/:id", orderController.findOne);
-router.put("/:id", orderController.updateStatus);
+router.patch("/:id", orderController.updateStatus);
 router.post("/payment/:id", orderController.payment);
 
 module.exports = router;
--- services/orderService.js
+++ services/orderService.js
@@ -1,9 +1,60 @@
-const findAll = async (params) => {};
+const prisma = require('../lib/prisma')
 
-const findOne = async (params) => {};
+const createOrder = async (userId, orderData) => {
+  const { promoId, courier, addressId, shippingCost, totalCost, deductionCost, netPrice, orderDetails } = orderData;
 
-const update = async (params) => {};
+  const order = await prisma.order.create({
+    data: {
+      userId,
+      promoId,
+      courier,
+      addressId,
+      shippingCost,
+      totalCost,
+      deductionCost,
+      netPrice,
+      orderDetails: {
+        create: orderDetails
+      }
+    },
+  });
 
-const payment = async (params) => {};
+  return order;
+};
 
-module.exports = { findAll, findOne, update, payment };
+const findAllOrders = async (userId) => {
+  const orders = await prisma.order.findMany({
+    where: { userId },
+    include: { orderDetails: true },
+  });
+  return orders;
+};
+
+const findOrderById = async (orderId) => {
+  const order = await prisma.order.findUnique({
+    where: { id: orderId },
+    include: { orderDetails: true },
+  });
+  return order;
+};
+
+const updateOrderStatus = async (orderId, status) => {
+  const order = await prisma.order.update({
+    where: { id: orderId },
+    data: { status },
+  });
+  return order;
+};
+
+const updatePaymentReceipt = async (orderId, paymentReceipt) => {
+  const order = await prisma.order.update({
+    where: { id: orderId },
+    data: { paymentReceipt, status: 'payment_verified' },
+  });
+  return order;
+};
+
+module.exports = {createOrder,findAllOrders,findOrderById,updateOrderStatus,updatePaymentReceipt};
+
+
+
