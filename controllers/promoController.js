d05fc263ec982be93c081bea67a6f9ef84b0b402
pradiknatama <pradiknatama@gmail.com>
2024-07-22 21:48:22 +0700

    add feature apply promo

--- controllers/promoController.js
+++ controllers/promoController.js
@@ -1,7 +1,18 @@
+const promoService = require('../services/promoService')
+
 const findAll = async (req, res, next) => {};
 
 const findOne = async (req, res, next) => {};
 
-const apply = async (req, res, next) => {};
+const apply = async (req, res, next) => {
+    try {
+        const params = { user_id: req.loggedUser.id, body: req.body};
+        const promo = await promoService.findOneByCode(params);
+        res.status(200).json({ message: "promo : ", data: promo });
+
+    } catch (error) {
+        next(error)
+    }
+};
 
 module.exports = { findAll, findOne, apply };
--- services/promoService.js
+++ services/promoService.js
@@ -22,6 +22,71 @@ const findOne = async (params) => {
   });
   return promo;
 };
+const findOneByCode = async (params) => {
+  const {promo_code} = params.body;
+  const promo = await prisma.promo.findUnique({
+    where :{
+      code: promo_code
+    }
+  });
+  if (!promo) {
+    throw { name: "ErrorNotFound", message: "Promo Not Found" };
+  }
+  let cart = await prisma.cart.findUnique({
+    where: { user_id: Number(params.user_id) },
+  });
+  
+  
+  const params_promo = {
+    promo:promo,
+    cart_id:cart.id
+  };
+  const validate = await validatePromo(params_promo);
+  const cart_update = await prisma.cart.update({
+    where: {
+      id: cart.id,
+    },
+    data: {
+      promo_id: promo.id,
+      promo_code:promo_code
+    },
+  });
+  return cart_update;
+};
+const validatePromo = async(params)=>{
+  const {promo,cart_id} = params;
+  let cart_Details = await prisma.cart_Detail.findMany({
+    where:{cart_id:cart_id}
+  });
+  if(promo.start_date!=null && promo.end_date!=null){
+    if(!(promo.start_date<= new Date() && promo.end_date>= new Date())){
+      throw { name: "ErrorNotFound", message:"Promo Expired" };
+    }
+  }else if(promo.start_date!=null){
+    if(!(promo.start_date<= new Date())){
+      throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+    }
+  }
+  if(promo.quantity==0){
+    throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+  }
+  if(promo.all_products==false){
+    if (cart_Details.length > 0) {
+      for (let i = 0; i < cart_Details.length; i++) {
+        const check_promo_product = await prisma.product_Promo.findFirst({
+          where: {
+            promo_id:promo.id,
+            product_id:cart_Details[i].product_id
+          }
+        })
+        if(!check_promo_product){
+          throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+        }
+      }
+    }
+  }
+  return true;
+}
 
 const create = async (params) => {
   const {
@@ -82,4 +147,4 @@ const update = async (params) => {
 
 const destroy = async (params) => {};
 
-module.exports = { findAll, findOne, create, update, destroy };
+module.exports = { findAll, findOne, create, update, findOneByCode, destroy };
