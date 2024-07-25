d06a44f78f1359985894cf22a3f0e1a32af94de5
akimajid <akimajid07@gmail.com>
2024-07-25 19:51:00 +0700

    Fix syntax

--- services/addressService.js
+++ services/addressService.js
@@ -3,124 +3,89 @@ const prisma = new PrismaClient();
 
 const findAll = async (params) => {
   const { userId } = params;
-  try {
-    const addresses = await prisma.address.findMany({
-      where: {
-        user_id: userId,
-      },
-    });
-    return addresses;
-  } catch (error) {
-    throw {
-      name: "AddressError",
-      message: `Error while fetching addresses for user ${userId}`,
-    };
-  }
+  const addresses = await prisma.address.findMany({
+    where: {
+      user_id: userId,
+    },
+  });
+  return addresses;
 };
 
 const findOne = async (params) => {
   const { addressId, userId } = params;
-  try {
-    const address = await prisma.address.findFirst({
-      where: {
-        id: addressId,
-        user_id: userId,
-      },
-    });
-    if (!address) {
-      throw {
-        name: "AddressNotFound",
-        message: `Address with ID ${addressId} not found or unauthorized.`,
-      };
-    }
-    return address;
-  } catch (error) {
-    if (error.name !== "AddressNotFound") {
-      throw {
-        name: "AddressError",
-        message: `Error while fetching address with ID ${addressId} for user ${userId}`,
-      };
-    }
-    throw error;
+  const address = await prisma.address.findFirst({
+    where: {
+      id: addressId,
+      user_id: userId,
+    },
+  });
+  if (!address) {
+    throw {
+      name: "AddressNotFound",
+      message: `Address with ID ${addressId} not found or unauthorized.`,
+    };
   }
+  return address;
 };
 
 const create = async (params) => {
   const { newAddress } = params;
-  try {
-    const createdAddress = await prisma.address.create({
-      data: {
-        title: newAddress.title,
-        description: newAddress.description,
-        zip_code: newAddress.zip_code,
-        city_id: newAddress.city_id,
-        province_id: newAddress.province_id,
-        user_id: newAddress.user_id,
-      },
-    });
-    return createdAddress;
-  } catch (error) {
-    throw { name: "AddressError", message: "Error while creating address" };
-  }
+  const createdAddress = await prisma.address.create({
+    data: {
+      title: newAddress.title,
+      description: newAddress.description,
+      zip_code: newAddress.zip_code,
+      city_id: newAddress.city_id,
+      province_id: newAddress.province_id,
+      user_id: newAddress.user_id,
+    },
+  });
+  return createdAddress;
 };
 
 const update = async (params) => {
   const { addressId, updatedFields, userId } = params;
-  try {
-    const address = await prisma.address.findFirst({
-      where: {
-        id: addressId,
-        user_id: userId,
-      },
-    });
-    if (!address) {
-      throw {
-        name: "AddressNotFound",
-        message: `Address with ID ${addressId} not found or unauthorized`,
-      };
-    }
-    const updatedAddress = await prisma.address.update({
-      where: {
-        id: addressId,
-      },
-      data: updatedFields,
-    });
-    return updatedAddress;
-  } catch (error) {
-    if (error.name !== "AddressNotFound") {
-      throw { name: "AddressError", message: "Error while updating address" };
-    }
-    throw error;
+  const address = await prisma.address.findFirst({
+    where: {
+      id: addressId,
+      user_id: userId,
+    },
+  });
+  if (!address) {
+    throw {
+      name: "AddressNotFound",
+      message: `Address with ID ${addressId} not found or unauthorized`,
+    };
   }
+  const updatedAddress = await prisma.address.update({
+    where: {
+      id: addressId,
+    },
+    data: updatedFields,
+  });
+  return updatedAddress;
 };
 
 const destroy = async (params) => {
   const { addressId, userId } = params;
-  try {
-    const address = await prisma.address.findFirst({
-      where: {
-        id: addressId,
-        user_id: userId,
-      },
-    });
-    if (!address) {
-      throw {
-        name: "AddressNotFound",
-        message: `Address with ID ${addressId} not found or unauthorized`,
-      };
-    }
-    const deletedAddress = await prisma.address.delete({
-      where: {
-        id: addressId,
-      },
-    });
-    return deletedAddress;
-  } catch (error) {
-    if (error.name !== "AddressNotFound") {
-      throw { name: "AddressError", message: "Error while deleting address" };
-    }
-    throw error;
+  const address = await prisma.address.findFirst({
+    where: {
+      id: addressId,
+      user_id: userId,
+    },
+  });
+  if (!address) {
+    throw {
+      name: "AddressNotFound",
+      message: `Address with ID ${addressId} not found or unauthorized`,
+    };
   }
+  const deletedAddress = await prisma.address.delete({
+    where: {
+      id: addressId,
+    },
+  });
+  return deletedAddress;
 };
 
 module.exports = { findAll, findOne, create, update, destroy };
--- services/cartService.js
+++ services/cartService.js
@@ -1,6 +1,6 @@
 const prisma = require("../lib/prisma");
 require("dotenv").config();
-const axios = require('axios');
+const axios = require("axios");
 
 const findOne = async (params) => {
   const user_id = params.user_id;
@@ -25,66 +25,79 @@ const reset = async (params) => {
   }
   return message;
 };
-const update = async (params)=>{
-  await prisma.$transaction( async (prisma)=>{
-    let shipping_cost=total_weight=shiping_cost=total_cost=net_price=deduction_cost=deduction_percent=0;
+const update = async (params) => {
+  await prisma.$transaction(async (prisma) => {
+    let shipping_cost =
+      (total_weight =
+      shiping_cost =
+      total_cost =
+      net_price =
+      deduction_cost =
+      deduction_percent =
+        0);
 
-    const { product_id, quantity, addres_id, courier_name, shipping_method, promo_code } = params.req.body;
-    let promo_id=null;
+    const {
+      product_id,
+      quantity,
+      addres_id,
+      courier_name,
+      shipping_method,
+      promo_code,
+    } = params.req.body;
+    let promo_id = null;
     let cart = await prisma.cart.findUnique({
       where: { user_id: Number(params.user_id) },
     });
     let affiliate = await prisma.affiliate.findUnique({
-      where: { user_id: Number(params.user_id),status:true },
+      where: { user_id: Number(params.user_id), status: true },
     });
-    if(promo_code !=null){
+    if (promo_code != null) {
       let promo = await prisma.promo.findUnique({
-        where: { 
-          code:promo_code,
+        where: {
+          code: promo_code,
         },
       });
-      if(promo==null){
-        throw { name: "ErrorNotFound",message:"Promo Not Found" };
-      }else{
-        if(promo.start_date!=null && promo.end_date!=null){
-          if(!(promo.start_date<= new Date() && promo.end_date>= new Date())){
-            throw { name: "ErrorNotFound", message:"Promo Expired" };
+      if (promo == null) {
+        throw { name: "ErrorNotFound", message: "Promo Not Found" };
+      } else {
+        if (promo.start_date != null && promo.end_date != null) {
+          if (
+            !(promo.start_date <= new Date() && promo.end_date >= new Date())
+          ) {
+            throw { name: "ErrorNotFound", message: "Promo Expired" };
           }
-        }else if(promo.start_date!=null){
-          if(!(promo.start_date<= new Date())){
-            throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+        } else if (promo.start_date != null) {
+          if (!(promo.start_date <= new Date())) {
+            throw { name: "ErrorNotFound", message: "Cant Use this Promo" };
           }
         }
-        if(promo.quantity==0){
-          throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+        if (promo.quantity == 0) {
+          throw { name: "ErrorNotFound", message: "Cant Use this Promo" };
         }
-        deduction_percent=promo.deduction;
+        deduction_percent = promo.deduction;
         promo_id = promo.id;
       }
     }
     // for checking this product already at cart or not
-    const check_cart_product= await prisma.cart_Detail.findFirst({
-      where: 
-      { 
+    const check_cart_product = await prisma.cart_Detail.findFirst({
+      where: {
         cart_id: Number(cart.id),
-        product_id:product_id
+        product_id: product_id,
       },
-
     });
     // get product
     const product = await prisma.product.findUnique({
-      where:{id: product_id}
+      where: { id: product_id },
     });
     // if not then create cart
-    if(check_cart_product==null){
-     
-      if(product == null){
+    if (check_cart_product == null) {
+      if (product == null) {
         throw { name: "ErrorNotFound" };
       }
-      if(quantity>0){
+      if (quantity > 0) {
         const cart_Detail = await prisma.cart_Detail.create({
           data: {
-            cart_id:  Number(cart.id),
+            cart_id: Number(cart.id),
             product_id: product_id,
             quantity: quantity,
             price: product.price,
@@ -92,8 +105,8 @@ const update = async (params)=>{
           },
         });
       }
-    }else{
-      if(quantity>0){
+    } else {
+      if (quantity > 0) {
         const cart_Detail = await prisma.cart_Detail.update({
           where: {
             id: Number(check_cart_product.id),
@@ -102,19 +115,17 @@ const update = async (params)=>{
             quantity: quantity,
             price: product.price,
             updated_at: new Date(),
-            created_at:check_cart_product.created_at
+            created_at: check_cart_product.created_at,
           },
         });
-      }else{
+      } else {
         const cart_detail = await prisma.cart_Detail.delete({
           where: { cart_id: cart.id },
         });
       }
-      
     }
     let cart_details = await prisma.cart_Detail.findMany({
-      where: 
-      { 
+      where: {
         cart_id: Number(cart.id),
       },
       include: {
@@ -122,20 +133,19 @@ const update = async (params)=>{
       },
     });
     if (cart_details.length > 0) {
-      cart_details.forEach(function(row){
-        total_weight += (row.quantity * row.product.weight);
-        total_cost += (row.quantity * row.product.price);
-
+      cart_details.forEach(function (row) {
+        total_weight += row.quantity * row.product.weight;
+        total_cost += row.quantity * row.product.price;
       });
     }
     const address = await prisma.address.findUnique({
-      where:{
-        id:addres_id
-      }
+      where: {
+        id: addres_id,
+      },
     });
     // for params rajaongkir
     const shipping_cost_params = {
-      city_id:address.city_id,
+      city_id: address.city_id,
       total_weight,
       courier_name,
       shipping_method,
@@ -146,99 +156,109 @@ const update = async (params)=>{
     }
     // deductioncost from total_cost * (affiliate+promo)/100
     // 50% + 10%
-    deduction_cost = total_cost * (affiliate.deduction+deduction_percent)/100;
+    deduction_cost =
+      (total_cost * (affiliate.deduction + deduction_percent)) / 100;
     // Calculate net price = total cost + shipping cost -deduction_cost
-    net_price = total_cost + shipping_cost-deduction_cost;
+    net_price = total_cost + shipping_cost - deduction_cost;
     const update_cart = await prisma.cart.update({
       where: { user_id: Number(params.user_id) },
-      data:{
-        shipping_cost:shipping_cost,
+      data: {
+        shipping_cost: shipping_cost,
         address_id: address.id,
-        total_cost:total_cost,
-        promo_code:(promo_code!=null?promo_code:null),
-        promo_id:promo_id,
-        deduction_cost:deduction_cost,
-        net_price:net_price,
-        created_at:cart.created_at,
+        total_cost: total_cost,
+        promo_code: promo_code != null ? promo_code : null,
+        promo_id: promo_id,
+        deduction_cost: deduction_cost,
+        net_price: net_price,
+        created_at: cart.created_at,
         updated_at: new Date(),
-      }
-    })
+      },
+    });
     if (!update_cart) {
       throw { name: "ErrorUpdate", message: "Failed to Update Cart" };
     }
 
     return update_cart;
-  })
-}
-const validatePromo = async(params)=>{
-  const {promo,cart_id} = params;
-  let cart_Details = await prisma.cart_Detail.findMany({
-    where:{cart_id:cart_id}
   });
-  if(promo.start_date!=null && promo.end_date!=null){
-    if(!(promo.start_date<= new Date() && promo.end_date>= new Date())){
-      throw { name: "ErrorNotFound", message:"Promo Expired" };
+};
+const validatePromo = async (params) => {
+  const { promo, cart_id } = params;
+  let cart_Details = await prisma.cart_Detail.findMany({
+    where: { cart_id: cart_id },
+  });
+  if (promo.start_date != null && promo.end_date != null) {
+    if (!(promo.start_date <= new Date() && promo.end_date >= new Date())) {
+      throw { name: "ErrorNotFound", message: "Promo Expired" };
     }
-  }else if(promo.start_date!=null){
-    if(!(promo.start_date<= new Date())){
-      throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+  } else if (promo.start_date != null) {
+    if (!(promo.start_date <= new Date())) {
+      throw { name: "ErrorNotFound", message: "Cant Use this Promo" };
     }
   }
-  if(promo.quantity==0){
-    throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+  if (promo.quantity == 0) {
+    throw { name: "ErrorNotFound", message: "Cant Use this Promo" };
   }
-  if(promo.all_products==false){
+  if (promo.all_products == false) {
     if (cart_Details.length > 0) {
       for (let i = 0; i < cart_Details.length; i++) {
         const check_promo_product = await prisma.product_Promo.findFirst({
           where: {
-            promo_id:promo.id,
-            product_id:cart_Details[i].product_id
-          }
-        })
-        if(!check_promo_product){
-          throw { name: "ErrorNotFound", message:"Cant Use this Promo" };
+            promo_id: promo.id,
+            product_id: cart_Details[i].product_id,
+          },
+        });
+        if (!check_promo_product) {
+          throw { name: "ErrorNotFound", message: "Cant Use this Promo" };
         }
       }
     }
   }
   return true;
-}
+};
 const getShippingCost = async (params) => {
-    try {
-        const { city_id, total_weight, courier_name, shipping_method, store_city_id } = params;
-        const response = await axios.post(
-            'https://api.rajaongkir.com/starter/cost',
-            {
-                origin: store_city_id,
-                destination: city_id,
-                weight: total_weight,
-                courier: courier_name
-            },
-            {
-                headers: {
-                    key: process.env.RAJAONGKIR_SECRET_KEY,
-                    'content-type': 'application/x-www-form-urlencoded'
-                }
-            }
-        );
-        // Get all shipping method
-        const courier_shipping_methods = response.data.rajaongkir.results[0].costs.map(cost => cost.service);
-        // Check if shipping method is available
-        if (!courier_shipping_methods.includes(shipping_method)) {
-            throw ({ name: "ErrorNotFound" })
-        } else {
-            // Get shipping cost based on shipping method
-            const shipping_cost = response.data.rajaongkir.results[0].costs.find(cost => cost.service == shipping_method);
-            return shipping_cost.cost[0].value;
-        }
-    } catch (error) {
-        if (error.name && error.message) {
-            throw error;
-        } else {
-            throw { name: "ErrorFetch", message: "Error Fetching Shipping Cost" };
-        }
+  try {
+    const {
+      city_id,
+      total_weight,
+      courier_name,
+      shipping_method,
+      store_city_id,
+    } = params;
+    const response = await axios.post(
+      "https://api.rajaongkir.com/starter/cost",
+      {
+        origin: store_city_id,
+        destination: city_id,
+        weight: total_weight,
+        courier: courier_name,
+      },
+      {
+        headers: {
+          key: process.env.RAJAONGKIR_SECRET_KEY,
+          "content-type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    // Get all shipping method
+    const courier_shipping_methods =
+      response.data.rajaongkir.results[0].costs.map((cost) => cost.service);
+    // Check if shipping method is available
+    if (!courier_shipping_methods.includes(shipping_method)) {
+      throw { name: "ErrorNotFound" };
+    } else {
+      // Get shipping cost based on shipping method
+      const shipping_cost = response.data.rajaongkir.results[0].costs.find(
+        (cost) => cost.service == shipping_method
+      );
+      return shipping_cost.cost[0].value;
     }
+  } catch (error) {
+    if (error.name && error.message) {
+      throw error;
+    } else {
+      throw { name: "ErrorFetch", message: "Error Fetching Shipping Cost" };
+    }
+  }
 };
 const deleteProduct = async (params) => {};
 
