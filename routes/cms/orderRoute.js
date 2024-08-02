e1a6afaf66235b60ad7689ca79c595590aec9075
pradiknatama <pradiknatama@gmail.com>
2024-08-02 22:39:24 +0700

    add authorization

--- routes/cms/orderRoute.js
+++ routes/cms/orderRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../../controllers/cms/orderController");
+const { authorization } = require("../../middlewares/auth");
 
-router.post("/",orderController.create);
-router.get("/", orderController.findAll);
-router.get("/:id", orderController.findOne);
-router.put("/:id/status", orderController.update);
-router.put("/:id/payment-receipt", orderController.payment);
+router.post("/", authorization(["admin"]),orderController.create);
+router.get("/", authorization(["admin"]), orderController.findAll);
+router.get("/:id", authorization(["admin"]), orderController.findOne);
+router.put("/:id/status", authorization(["admin"]), orderController.update);
+router.put("/:id/payment-receipt", authorization(["admin"]), orderController.payment);
 
 
 
